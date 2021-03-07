656ffe7476873dcdcbf55f31f954aff6dd9e12cd
teverus <62369371+teverus@users.noreply.github.com>
2021-03-07 10:29:54 +0300

    Add files via upload

---
+++ _email_related_functions.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from time import sleep
+
+
+def send_email(recipient_email, subject, email_body):
+    SENDER_EMAIL = "TeverusAutomation@gmail.com"
+    SENDER_PASSWORD = "HailSatan666"
+
+    message = MIMEMultipart()
+    message['To'] = recipient_email
+    message['From'] = SENDER_EMAIL
+    message['Subject'] = subject
+
+    message.attach(MIMEText(email_body, "html"))
+
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls()
+    session.login(SENDER_EMAIL, SENDER_PASSWORD)
+    session.sendmail(SENDER_EMAIL, recipient_email, message.as_string())
+    session.quit()
+
+    sleep(4)
